ce1ffa399bf8fc7ead4da71ee0042c08daa408c5
JuaniCama <juanignacio.cama@correo.ucu.edu.uy>
2024-11-25 20:52:55 -0300

    Comentario de lo hecho.

--- app/index.tsx
+++ app/index.tsx
@@ -4,3 +4,7 @@ import AppNavigator from '../navigation/AppNavigator';
 export default function App() {
   return <AppNavigator />;
 }
+/*
+Pude implementar todo, lo unico que hice distinto fue utilizar un "picker" en vez de una "checkbox" para seleccionar la dificultad de destination para que se vea mas bonito.
+Para la parte que pide Responsividad, hice que la pagina de destinations se vea siempre al 85% del tamaño de la pantalla en general.
+*/
